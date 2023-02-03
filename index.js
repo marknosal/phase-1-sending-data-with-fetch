7fdecbc4b4f1c9b1e2bab1fffe1fac07bddf6242
Mark Nosal <mark.c.nosal@gmail.com>
2023-02-02 21:29:45 -0600

    This was a toughy

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
 // Add your code here
+// const formData = {
+//     dogName: 'Byron',
+//     dogBreed: 'Poodle', 
+// }
+
+// const configurationObject = {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json',
+//     },
+//     body: JSON.stringify(formData),
+// };
+
+// fetch('http://localhost:3000/dogs', configurationObject)
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(function (error) {
+//         alert("Bad things! Ragnarők!");
+//         console.log(error.message);
+//     });
+
+// const postBody = {
+//     name: 'Chuy',
+//     email: 'tripod3leg@yahoo.com',
+// };
+
+// const postObject = {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json',
+//     },
+//     body: JSON.stringify(postBody),
+// };
+
+function submitData(name, email) {
+    const postBody = {
+        name: 'Steve',
+        email: 'steve@steve.com',
+    };
+
+    const postObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(postBody),
+    };
+    
+    return fetch('http://localhost:3000/users', postObject)
+        .then(response => response.json())
+        .then((data) => {
+            console.log(data.id);
+            const postResponse = document.querySelector('body').append(data.id)
+        })
+        .catch(function (error) {
+            const errorMessage = document.querySelector('body').append(error)
+        });
+    
+}
